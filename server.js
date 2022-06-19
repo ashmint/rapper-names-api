a7118b83b611c68287f1481f8efe560850c7fb3f
Ashish Tom <ashmint@gmail.com>
2022-06-19 20:52:57 +0530

    added git ignore

---
+++ server.js
@@ -0,0 +1,40 @@
+const { request } = require('express')
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+const rappers = {
+    '21 savage': {
+        'age' : 29,
+        'birthName': 'Sheyaa Bin Abraham-Joseph',
+        'birthLocation': 'London, England'
+    },
+    'chance the rapper': {
+        'age' : 29,
+        'birthName': 'chancelor Bennet',
+        'birthLocation': 'Chicago'
+    },
+    'dylan': {
+        'age' : 29,
+        'birthName': 'Dylan',
+        'birthLocation': 'Dylan'
+    }
+}
+
+app.get('/', (request, response)=> {
+    response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:rapperName', (request, response) =>{
+    let rapperName = request.params.rapperName.toLocaleLowerCase();
+    if (rappers[rapperName]){
+        response.json(rappers[rapperName])
+    }
+    else{
+        response.json(rappers['dylan'])
+    }
+})
+
+app.listen(PORT, ()=>{
+    console.log(`The server is running on ${PORT}!`)
+})
